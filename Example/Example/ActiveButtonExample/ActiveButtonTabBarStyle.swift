a501f0dc5455351783cb06a7ab44597aad703ec7
Daniil Sentsov <daniil_sencov@mail.ru>
2024-10-25 16:39:39 +0300

    Added ActiveShape struct

--- Example/Example/ActiveButtonExample/ActiveButtonTabBarStyle.swift
+++ Example/Example/ActiveButtonExample/ActiveButtonTabBarStyle.swift
@@ -27,45 +27,14 @@ struct ActiveButtonTabBarStyle: TabBarStyle {
             }
             .padding(.bottom, -26)
             ZStack {
-                backgroundShape
+                ActiveShape()
+                    .shadow(color: Color(UIColor.black.withAlphaComponent(0.3)), radius: 8)
                     .foregroundColor(Color.indigo)
                 itemsView()
             }
             .padding(.bottom, geometry.safeAreaInsets.bottom)
             .frame(height: 70 + geometry.safeAreaInsets.bottom)
         }
-    }
-    
-    var backgroundShape: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 0, y: 0))
-            
-            let startCurveX: CGFloat = 40 + 6
-            let endXOffset: CGFloat = 25 + 6
-            let endYOffset: CGFloat = 8
-            let curveX: CGFloat = 28 + 6
-            
-            path.addLine(to: CGPoint(x: UIScreen.main.bounds.size.width / 2 - startCurveX, y: 0))
-            
-            path.addCurve(to: CGPoint(x: UIScreen.main.bounds.size.width / 2 - endXOffset, y: endYOffset),
-                          control1: CGPoint(x: UIScreen.main.bounds.size.width / 2 - startCurveX, y: 0),
-                          control2: CGPoint(x: UIScreen.main.bounds.size.width / 2 - curveX, y: 0))
-            
-            path.addArc(center: CGPoint(x: UIScreen.main.bounds.size.width / 2, y: 0),
-                        radius: 26 + 6,
-                        startAngle: .degrees(160 - 15),
-                        endAngle: .degrees(20 + 15),
-                        clockwise: true)
-            
-            path.addCurve(to: CGPoint(x: UIScreen.main.bounds.size.width / 2 + startCurveX, y: 0),
-                          control1: CGPoint(x: UIScreen.main.bounds.size.width / 2 + endXOffset, y: endYOffset),
-                          control2: CGPoint(x: UIScreen.main.bounds.size.width / 2 + curveX, y: 0))
-            
-            path.addLine(to: CGPoint(x: UIScreen.main.bounds.size.width, y: 0))
-            path.addLine(to: CGPoint(x: UIScreen.main.bounds.size.width, y: 200))
-            path.addLine(to: CGPoint(x: 0, y: 200))
-            path.addLine(to: CGPoint(x: 0, y: 0))
-        }.shadow(color: Color(UIColor.black.withAlphaComponent(0.1)), radius: 8)
     }
     
 }
